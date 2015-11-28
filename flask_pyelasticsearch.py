d1dfad388c8d367ff6a2b84d8471a316d2db8d7f
Curlyfingers <curlyfingers.ua@gmail.com>
2015-11-28 11:39:54 +0200

    indents to spaces

--- flask_pyelasticsearch.py
+++ flask_pyelasticsearch.py
@@ -2,21 +2,21 @@ from pyelasticsearch import ElasticSearch as PyElasticSearch
 
 
 class ElasticSearch(object):
-	"""
-	A thin wrapper around pyelasticsearch.ElasticSearch()
-	"""
-	def __init__(self, app=None):
-		if app is not None:
-			self.init_app(app)
+    """
+    A thin wrapper around pyelasticsearch.ElasticSearch()
+    """
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
 
-	def init_app(self, app):
-		self.app = app
-		app.config.setdefault('ELASTICSEARCH_URL', 'http://localhost:9200/')
-		if not hasattr(app, 'extensions'):
-			app.extensions = {}
-		app.extensions['elasticsearch'] = PyElasticSearch(app.config['ELASTICSEARCH_URL'])
+    def init_app(self, app):
+        self.app = app
+        app.config.setdefault('ELASTICSEARCH_URL', 'http://localhost:9200/')
+        if not hasattr(app, 'extensions'):
+            app.extensions = {}
+        app.extensions['elasticsearch'] = PyElasticSearch(app.config['ELASTICSEARCH_URL'])
 
-	def __getattr__(self, item):
-		if 'elasticsearch' not in self.app.extensions.keys():
-			raise Exception('not initialised, did you forget to call init_app?')
-		return getattr(self.app.extensions['elasticsearch'], item)
+    def __getattr__(self, item):
+        if 'elasticsearch' not in self.app.extensions.keys():
+            raise Exception('not initialised, did you forget to call init_app?')
+        return getattr(self.app.extensions['elasticsearch'], item)
